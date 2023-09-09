9f07f258700eafa5f57cf815e502ea24d0c7e683
Killian Kelly <killiank360@gmail.com>
2023-09-09 12:04:19 +0100

    Added boilerplate code

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const CWN = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ CWN.abilities = {
+  "str": "CWN.AbilityStr",
+  "dex": "CWN.AbilityDex",
+  "con": "CWN.AbilityCon",
+  "int": "CWN.AbilityInt",
+  "wis": "CWN.AbilityWis",
+  "cha": "CWN.AbilityCha"
+};
+
+CWN.abilityAbbreviations = {
+  "str": "CWN.AbilityStrAbbr",
+  "dex": "CWN.AbilityDexAbbr",
+  "con": "CWN.AbilityConAbbr",
+  "int": "CWN.AbilityIntAbbr",
+  "wis": "CWN.AbilityWisAbbr",
+  "cha": "CWN.AbilityChaAbbr"
+};
