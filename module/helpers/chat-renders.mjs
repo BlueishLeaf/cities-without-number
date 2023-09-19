89a3b39ebce5a041fa9fcf78449e165503d6b21b
Killian Kelly <killiank360@gmail.com>
2023-09-19 16:16:15 +0100

    Added shock damage support

--- module/helpers/chat-renders.mjs
+++ module/helpers/chat-renders.mjs
@@ -1,32 +1,42 @@
 export const buildChatContentForAttackRoll = (weapon, damageRoll, rollRenders) => {
     let content = attackRender(rollRenders[0]);
 
-    // Add trauma render if it exists
-    if (rollRenders[2]) {
-      content += damageRenderWithTrauma(rollRenders[2], rollRenders[1], weapon.system.traumaRating, damageRoll.total);
-    } else {
-      content += damageRenderWithoutTrauma(rollRenders[1]);
+    // Add trauma and shock renders if applicable
+    content += rollRenders[2] ? damageRenderWithTrauma(rollRenders[2], rollRenders[1], weapon.system.traumaRating, damageRoll.total) : damageRenderWithoutTrauma(rollRenders[1]);
+    
+    if (weapon.system.shockDamage && weapon.system.shockThreshold) {
+        content += shockDamageRender(weapon);
     }
 
     return content;
 }
 
+export const shockDamageRender = (weapon) => `
+    <h4>Shock Damage</h4>
+    <div class="dice-roll">
+        <div class="dice-formula">AC <= ${weapon.system.shockThreshold}</div>
+        <div class="dice-result">
+            <h4 class="dice-total">${weapon.system.shockDamage}</h4>
+        </div>
+    </div>
+`;
+
 export const damageRenderWithTrauma = (traumaRollRender, damageRollRender, traumaRating, damageRollTotal) => `
     <h4>Trauma Roll (x${traumaRating} Damage on Traumatic Hit)</h4>
-    ${traumaRollRender}<br>
+    ${traumaRollRender}
     <div class="grid grid-2col">
         <div class="flex-group-left">
-        <h4>Damage (Normal)</h4>
-        ${damageRollRender}
+            <h4>Damage (Normal)</h4>
+            ${damageRollRender}
         </div>
         <div class="flex-group-left">
-        <h4>Damage (Traumatic)</h4>
-        <div class="dice-roll">
-            <div class="dice-formula">${traumaRating} * ${damageRollTotal}</div>
-            <div class="dice-result">
-            <h4 class="dice-total">${traumaRating * damageRollTotal}</h4>
+            <h4>Damage (Traumatic)</h4>
+            <div class="dice-roll">
+                <div class="dice-formula">${traumaRating} * ${damageRollTotal}</div>
+                <div class="dice-result">
+                    <h4 class="dice-total">${traumaRating * damageRollTotal}</h4>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 `;
@@ -37,6 +47,8 @@ export const damageRenderWithoutTrauma = (damageRollRender) => `
 `;
 
 export const attackRender = (attackRollRender) =>`
-    <h4>Attack Roll</h4>
-    ${attackRollRender}<br>
+    <div style="margin: 10px 0;">
+        <h4>Attack Roll</h4>
+        ${attackRollRender}
+    </div>
 `;
