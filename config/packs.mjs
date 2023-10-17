893bf2a2d641c7707b78b439f338bedb7dcf4a8c
Killian Kelly <killiank360@gmail.com>
2023-10-17 19:18:36 +0100

    Updated pack config

--- config/packs.mjs
+++ config/packs.mjs
@@ -15,14 +15,13 @@ import yargs from "yargs";
 const parsedArgs = yargs(process.argv).argv;
 
 /**
- * Folder where the compiled compendium packs should be located relative to the
- * base 5e system folder.
+ * Folder where the compiled compendium packs should be located relative to the base CWN system folder.
  * @type {string}
  */
 const PACK_DEST = "packs";
 
 /**
- * Folder where source JSON files should be located relative to the 5e system folder.
+ * Folder where source JSON files should be located relative to the CWN system folder.
  * @type {string}
  */
 const PACK_SRC = "packs/src";
@@ -182,48 +181,3 @@ function compilePacks() {
   return mergeStream(packs);
 }
 export const compile = compilePacks;
-
-
-/* ----------------------------------------- */
-/*  Extract Packs
-/* ----------------------------------------- */
-
-/**
- * Extract the contents of compendium packs to JSON files.
- *
- * - `gulp extractPacks` - Extract all compendium NEDB files into JSON files.
- * - `gulp extractPacks --pack classes` - Only extract the contents of the specified compendium.
- * - `gulp extractPacks --pack classes --name Barbarian` - Only extract a single item from the specified compendium.
- */
-function extractPacks() {
-  const packName = parsedArgs.pack ?? "*";
-  const entryName = parsedArgs.name?.toLowerCase();
-  const packs = gulp.src(`${PACK_DEST}/**/${packName}.db`)
-    .pipe(through2.obj((file, enc, callback) => {
-      const filename = path.parse(file.path).name;
-      const folder = path.join(PACK_SRC, filename);
-      if ( !fs.existsSync(folder) ) fs.mkdirSync(folder, { recursive: true, mode: 0o775 });
-
-      const db = new Datastore({ filename: file.path, autoload: true });
-      db.loadDatabase();
-
-      db.find({}, (err, entries) => {
-        entries.forEach(entry => {
-          const name = entry.name.toLowerCase();
-          if ( entryName && (entryName !== name) ) return;
-          cleanPackEntry(entry);
-          const output = `${JSON.stringify(entry, null, 2)}\n`;
-          const outputName = name.replace("'", "").replace(/[^a-z0-9]+/gi, " ").trim().replace(/\s+|-{2,}/g, "-");
-          const subfolder = folder;
-          if ( !fs.existsSync(subfolder) ) fs.mkdirSync(subfolder, { recursive: true, mode: 0o775 });
-          fs.writeFileSync(path.join(subfolder, `${outputName}.json`), output, { mode: 0o664 });
-        });
-      });
-
-      logger.info(`Extracting pack ${filename}`);
-      callback(null, file);
-    }));
-
-  return mergeStream(packs);
-}
-export const extract = extractPacks;
--- gulpfile.mjs
+++ gulpfile.mjs
@@ -20,11 +20,11 @@ export const lint = gulp.series(javascript.lint);
 // Compendium pack management
 export const cleanPacks = gulp.series(packs.clean);
 export const compilePacks = gulp.series(packs.compile);
-export const extractPacks = gulp.series(packs.extract);
 
 // Build all artifacts
-export const buildAll = gulp.parallel(
+export const buildAll = gulp.series(
   css.compile,
   javascript.compile,
+  packs.clean,
   packs.compile
 );
