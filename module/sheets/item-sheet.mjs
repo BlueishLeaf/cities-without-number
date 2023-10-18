8bf133a911325c9b80640f301fbf3669459c2894
Killian Kelly <killiank360@gmail.com>
2023-10-18 21:10:51 +0100

    Added armor mod support

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -10,7 +10,8 @@ export class CitiesWithoutNumberItemSheet extends ItemSheet {
       classes: ["cwn", "sheet", "item"],
       width: 520,
       height: 480,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
+      dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
     });
   }
 
@@ -45,6 +46,7 @@ export class CitiesWithoutNumberItemSheet extends ItemSheet {
     // Add the actor's data to context.data for easier access, as well as flags.
     context.system = itemData.system;
     context.flags = itemData.flags;
+    context.mods = itemData.system.mods;
 
     return context;
   }
@@ -55,9 +57,158 @@ export class CitiesWithoutNumberItemSheet extends ItemSheet {
   activateListeners(html) {
     super.activateListeners(html);
 
+    // Render the item sheet for viewing/editing prior to the editable check.
+    html.find(".item-edit").click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      const item = this.actor.items.get(li.data("itemId"));
+      item.sheet.render(true);
+    });
+
     // Everything below here is only needed if the sheet is editable
     if (!this.isEditable) return;
 
     // Roll handlers, click handlers, etc. would go here.
+
+    // Add Inventory Item
+    html.find(".item-create").click(this._onItemCreate.bind(this));
+
+    // Delete Inventory Item
+    html.find(".item-delete").click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      const item = this.actor.items.get(li.data("itemId"));
+      item.delete();
+      this.item.system.mods.splice(this.item.system.mods.indexOf(item), 1);
+      Item.updateDocuments([{_id: this.item._id, system: { mods: this.item.system.mods }}], {parent: this.actor}).then(updates => console.log("Updated item", updates));
+    });
+
+    // Drag events for macros.
+    if (this.actor.isOwner) {
+      let handler = ev => this._onDragStart(ev);
+      html.find("li.item").each((i, li) => {
+        if (li.classList.contains("inventory-header")) return;
+        li.setAttribute("draggable", true);
+        li.addEventListener("dragstart", handler, false);
+      });
+    }
+  }
+
+  _canDragStart(_selector) {
+    return this.isEditable;
+  }
+
+  _canDragDrop(_selector) {
+    return this.isEditable;
+  }
+
+  _onDragStart(event) {
+    const li = event.currentTarget;
+    if ( event.target.classList.contains("content-link") ) return;
+
+    // Create drag data
+    let dragData;
+
+    // Owned Items
+    if ( li.dataset.itemId ) {
+      const item = this.actor.items.get(li.dataset.itemId);
+      dragData = item.toDragData();
+    }
+
+    if ( !dragData ) return;
+
+    // Set data transfer
+    event.dataTransfer.setData("text/plain", JSON.stringify(dragData));
+  }
+
+  async _onDrop(event) {
+    const data = TextEditor.getDragEventData(event);
+    const actor = this.actor;
+    const allowed = Hooks.call("dropActorSheetData", actor, this, data);
+    if ( allowed === false ) return;
+    if ( data.type !== "Item" ) return;
+
+    return this._onDropItem(event, data);
+  }
+
+  async _onDropItem(event, data) {
+    if ( !this.actor.isOwner ) return false;
+    const item = await Item.implementation.fromDropData(data);
+    const itemData = item.toObject();
+
+    // Handle item sorting within the same Actor
+    if ( this.actor.uuid === item.parent?.uuid ) return this._onSortItem(event, itemData);
+
+    // Create the owned item
+    return this._onDropItemCreate(itemData);
+  }
+
+  async _onDropItemCreate(itemData) {
+    itemData = itemData instanceof Array ? itemData : [itemData];
+    const newItems = await this.actor.createEmbeddedDocuments("Item", itemData);
+    this.item.system.mods.push(newItems[0]);
+    Item.updateDocuments([{_id: this.item._id, system: { mods: this.item.system.mods }}], {parent: this.actor}).then(updates => console.log("Updated item", updates));
+    return newItems;
+  }
+
+  _onSortItem(event, itemData) {
+    console.info("sorting", event, itemData);
+    // Get the drag source and drop target
+    const items = this.actor.items;
+    const source = items.get(itemData._id);
+    const dropTarget = event.target.closest("[data-item-id]");
+    if ( !dropTarget ) return;
+    console.info("dropTarget", dropTarget);
+    console.info("items", items);
+    const target = items.get(dropTarget.dataset.itemId);
+    console.info("target", target);
+
+    // Don't sort on yourself
+    if ( source.id === target.id ) return;
+
+    // Identify sibling items based on adjacent HTML elements
+    const siblings = [];
+    for ( let el of dropTarget.parentElement.children ) {
+      const siblingId = el.dataset.itemId;
+      if ( siblingId && (siblingId !== source.id) ) siblings.push(items.get(el.dataset.itemId));
+    }
+
+    // Perform the sort
+    const sortUpdates = SortingHelpers.performIntegerSort(source, {target, siblings});
+    const updateData = sortUpdates.map(u => {
+      const update = u.update;
+      update._id = u.target._id;
+      return update;
+    });
+
+    // Perform the update
+    return this.actor.updateEmbeddedDocuments("Item", updateData);
+  }
+
+  /**
+   * Handle creating a new Owned Item for the item using initial data defined in the HTML dataset
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  async _onItemCreate(event) {
+    event.preventDefault();
+    const header = event.currentTarget;
+    // Get the type of item to create.
+    const type = header.dataset.type;
+    // Grab any data associated with this control.
+    const data = duplicate(header.dataset);
+    // Initialize a default name.
+    const name = `New ${type.capitalize()}`;
+    // Prepare the item object.
+    const itemData = {
+      name: name,
+      type: type,
+      system: data
+    };
+    // Remove the type from the dataset since it's in the itemData.type prop.
+    delete itemData.system.type;
+
+    // Finally, create the item!
+    const createdItem = await Item.create(itemData, {parent: this.actor});
+    this.item.system.mods.push(createdItem);
+    Item.updateDocuments([{_id: this.item._id, system: { mods: this.item.system.mods }}], {parent: this.actor}).then(updates => console.log("Updated item", updates));
   }
 }
