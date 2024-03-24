4de762baf003728b3772af1347eff00c2e768536
Killian Kelly <killiank360@gmail.com>
2024-03-24 13:18:27 +0000

    Added area to write down goals, languages, and contacts

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -15,6 +15,7 @@ export const preloadHandlebarsTemplates = async function() {
     "systems/cities-without-number/templates/actor/parts/actor-vehicle-inventory.hbs",
     "systems/cities-without-number/templates/actor/parts/actor-cyberware.hbs",
     "systems/cities-without-number/templates/actor/parts/actor-features.hbs",
-    "systems/cities-without-number/templates/actor/parts/actor-biography.hbs"
+    "systems/cities-without-number/templates/actor/parts/actor-biography.hbs",
+    "systems/cities-without-number/templates/actor/parts/actor-character-biography.hbs"
   ]);
 };
