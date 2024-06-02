766192fcac7085d0fb0f2118d5365b3d1d6a4999
Killian Kelly <killiank360@gmail.com>
2024-06-02 18:25:46 +0100

    Renamed files

--- module/data/module.mjs
+++ module/data/module.mjs
@@ -1,8 +1,8 @@
 // Actor Data Models
-export {default as CharacterActorData} from "./actors/actor-character.mjs";
-export {default as NpcActorData} from "./actors/actor-npc.mjs";
-export {default as DroneActorData} from "./actors/actor-drone.mjs";
-export {default as VehicleActorData} from "./actors/actor-vehicle.mjs";
-export {default as ServerActorData} from "./actors/actor-server.mjs";
+export {default as CharacterActorData} from "./actors/character-actor.mjs";
+export {default as NpcActorData} from "./actors/npc-actor.mjs";
+export {default as DroneActorData} from "./actors/drone-actor.mjs";
+export {default as VehicleActorData} from "./actors/vehicle-actor.mjs";
+export {default as ServerActorData} from "./actors/server-actor.mjs";
 
 // Item Data Models
