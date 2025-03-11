bc6bc537400f998ce8ca9a60c4f5da2edd424c17
Killian Kelly <killiank360@gmail.com>
2025-03-11 18:29:26 +0000

    Removed unused fields on edge model

--- module/data/items/edge-item.mjs
+++ module/data/items/edge-item.mjs
@@ -1,13 +1,10 @@
 import {BaseItemData} from "./templates/base-item.mjs";
-import {integerField} from "../../helpers/schema-helpers.mjs";
 
 export default class EdgeItemData extends BaseItemData {
     static defineSchema() {
         const baseData = super.defineSchema();
         return {
-            ...baseData,
-            costPercentage: integerField(0),
-            specialTechRequired: integerField(0)
+            ...baseData
         }
     }
 }
