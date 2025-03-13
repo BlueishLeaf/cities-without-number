736c313ee231086ca4f5a1665df84313263cfd7b
Killian Kelly <killiank360@gmail.com>
2025-03-13 18:18:10 +0000

    Extracted handlebars helpers

--- module/cwn.mjs
+++ module/cwn.mjs
@@ -10,6 +10,10 @@ import {CWN} from "./helpers/config.mjs";
 // Import DataModel classes
 import * as models from './data/module.mjs';
 import {registerSettings} from "./helpers/settings.mjs";
+import registerHandlebarsHelpers from "./helpers/handlebars-helpers.mjs";
+
+// Register additional helpers
+registerHandlebarsHelpers();
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
@@ -90,53 +94,5 @@ Hooks.on("createActor", async (actor, _options, _userId) => {
     if (!CONFIG.CWN.actorsWithSkills.includes(actor.type)) return;
     const skillPack = game.packs.get("cities-without-number.skills");
     const skills = await skillPack.getDocuments({name__in: CONFIG.CWN.startingSkills});
-    actor.createEmbeddedDocuments("Item", skills);
-});
-
-/* -------------------------------------------- */
-/*  Handlebars Helpers                          */
-/* -------------------------------------------- */
-
-// If you need to add Handlebars helpers, here are a few useful examples:
-Handlebars.registerHelper("concat", function () {
-    let outStr = "";
-    for (let arg in arguments) {
-        if (typeof arguments[arg] != "object") {
-            outStr += arguments[arg];
-        }
-    }
-    return outStr;
-});
-
-Handlebars.registerHelper("toLowerCase", function (str) {
-    return str.toLowerCase();
-});
-
-Handlebars.registerHelper("isChecked", function (condition) {
-    return condition ? "checked" : "";
-});
-
-Handlebars.registerHelper("isArmorReadiable", function (selectedArmor, allArmor) {
-    let readiable = true;
-    if (selectedArmor.system.subType === 'accessory' && !selectedArmor.system.canEquipWithSuit && allArmor.find(armorItem => armorItem.system.subType === 'armor' && armorItem.system.readied && armorItem.system.isSuit)) {
-        readiable = false;
-    }
-    return readiable ? "" : "disabled title=\"Cannot equip this accessory while wearing an armored suit\"";
-});
-
-Handlebars.registerHelper("isSelected", function (condition) {
-    return condition ? "selected" : "";
-});
-
-Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
-    return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
-});
-
-Handlebars.registerHelper("isSettingEnabled", function (settingKey, options) {
-    return game.settings.get("cities-without-number", settingKey) ? options.fn(this) : options.inverse(this);
-});
-
-Handlebars.registerHelper("formatChromeDefect", function (defectId, defectCollection, headerClass) {
-    const defect = defectCollection.find(s => s.id === defectId);
-    return defect ? `<h4 class="${headerClass}"><b>${defect.name}:</b>${defect.system.description}</h4>` : '';
+    await actor.createEmbeddedDocuments("Item", skills);
 });
---
+++ module/helpers/handlebars-helpers.mjs
@@ -0,0 +1,44 @@
+export default function registerHandlebarsHelpers() {
+    Handlebars.registerHelper("concat", function () {
+        let outStr = "";
+        for (let arg in arguments) {
+            if (typeof arguments[arg] != "object") {
+                outStr += arguments[arg];
+            }
+        }
+        return outStr;
+    });
+
+    Handlebars.registerHelper("toLowerCase", function (str) {
+        return str.toLowerCase();
+    });
+
+    Handlebars.registerHelper("isChecked", function (condition) {
+        return condition ? "checked" : "";
+    });
+
+    Handlebars.registerHelper("canReadyArmor", function (selectedArmor, allArmor) {
+        let canReady = true;
+        if (selectedArmor.system.subType === 'accessory' && !selectedArmor.system.canEquipWithSuit && allArmor.find(armorItem => armorItem.system.subType === 'armor' && armorItem.system.readied && armorItem.system.isSuit)) {
+            canReady = false;
+        }
+        return canReady ? "" : "disabled title=\"Cannot equip this accessory while wearing an armored suit\"";
+    });
+
+    Handlebars.registerHelper("isSelected", function (condition) {
+        return condition ? "selected" : "";
+    });
+
+    Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
+        return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
+    });
+
+    Handlebars.registerHelper("isSettingEnabled", function (settingKey, options) {
+        return game.settings.get("cities-without-number", settingKey) ? options.fn(this) : options.inverse(this);
+    });
+
+    Handlebars.registerHelper("formatChromeDefect", function (defectId, defectCollection, headerClass) {
+        const defect = defectCollection.find(s => s.id === defectId);
+        return defect ? `<h4 class="${headerClass}"><b>${defect.name}:</b>${defect.system.description}</h4>` : '';
+    });
+}
